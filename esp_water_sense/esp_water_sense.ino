fe9501de8404c8762fd5d1e81532bf68ec9f5847
baycat <anyssa.cast@gmail.com>
2023-04-11 15:29:18 -0500

    six level resolution for water tracked digitally

--- esp_water_sense/esp_water_sense.ino
+++ esp_water_sense/esp_water_sense.ino
@@ -11,9 +11,12 @@
 #define water_sensor_7 A7
 #define water_sensor_8 A8
 
-float avgBottomVoltage = 0;
-float avgMidVoltage = 0;
-float avgTopVoltage = 0;
+bool lvl0 = 0;
+bool lvl1 = 0;
+bool lvl2 = 0;
+bool lvl3 = 0;
+bool lvl4 = 0;
+bool lvl5 = 0;
 
 
 void setup() {
@@ -21,19 +24,25 @@ void setup() {
   Serial.begin(115200);
   Serial.setTimeout(2000);
   
-  //top level sensors
+  //level 0
   pinMode(water_sensor_0,INPUT);
   pinMode(water_sensor_1,INPUT);
+
+  //level 1
   pinMode(water_sensor_2,INPUT);
 
-  //mid level sensors
+  //level 2
   pinMode(water_sensor_3,INPUT);
   pinMode(water_sensor_4,INPUT);
+
+  //level 3
   pinMode(water_sensor_5,INPUT);
 
-  //bottom level sensors
+  //level 4
   pinMode(water_sensor_6,INPUT);
   pinMode(water_sensor_7,INPUT);
+  
+  //level 5
   pinMode(water_sensor_8,INPUT);
 
 
@@ -45,21 +54,50 @@ void setup() {
 }
 
 void loop() {
-  
-  static unsigned long analogSampleTimepoint = millis();
-  if(millis()-analogSampleTimepoint > 40U){     //every 40 milliseconds,read the analog value from the ADC
-    analogSampleTimepoint = millis();
-    analogBuffer[analogBufferIndex] = analogRead(TdsSensorPin);    //read the analog value and store into the buffer
-    analogBufferIndex++;
-    if(analogBufferIndex == SCOUNT){ 
-      analogBufferIndex = 0;
-    }
-  //check each sensor and update average voltage level
-  avgBottomVoltage =
-  avgMidVoltage =
-  avgTopVoltage =
 
-  //convert average voltage to a water level
-  water_lvl = (133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5;
+  //every X seconds, check the levels
+  if time % X == Y ->
+
+    //check each sensor and update level boolean
+    // lvl 0 update
+    if water_sensor_0 || water_sensor_1
+      lvl0 = 1;
+    else 
+      lvl0 = 0;
+
+    // lvl 1 update
+    if water_sensor_2
+      lvl1 = 1;
+    else 
+      lvl1 = 0;
+
+    // lvl 2
+    if water_sensor_3 || water_sensor_4
+      lvl2 = 1;
+    else 
+      lvl2 = 0;
+
+    // lvl 3 update
+    if water_sensor_5
+      lvl2 = 1;
+    else 
+      lvl2 = 0;
+
+
+    // lvl 4 update
+    if water_sensor_6 || water_sensor_7
+      lvl2 = 1;
+    else 
+      lvl2 = 0;
+
+
+    // lvl 5 update
+    if water_sensor_8
+      lvl2 = 1;
+    else 
+      lvl2 = 0;
+
+
+
 
 }
